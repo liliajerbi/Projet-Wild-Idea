69d7460d7a7286dda50d155e4c19bfd2f8c5713c
Joséphine Godard <josephine.godard@gmail.com>
2019-03-14 09:11:20 +0100

    "AboutUs fait : manque plus que le texte à mettre"

---
+++ app/src/main/java/com/example/wildeas/AboutUs.java
@@ -0,0 +1,52 @@
+package com.example.wildeas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AboutUs extends AppCompatActivity {
+    private EditText mEditTextSubject;
+    private EditText mEditTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        mEditTextSubject = findViewById(R.id.edit_text_subject);
+        mEditTextMessage = findViewById(R.id.edit_text_message);
+        TextView creatMessage1 = findViewById(R.id.editText8);
+        creatMessage1.setMovementMethod(new ScrollingMovementMethod());
+        TextView creatMessage2 = findViewById(R.id.editText11);
+        creatMessage2.setMovementMethod(new ScrollingMovementMethod());
+
+        Button buttonSend = findViewById(R.id.button_send);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMail();
+            }
+        });
+    }
+
+    private void sendMail(){
+        String recipientList = "razouk5@gmail.com";
+        String[] recipients = recipientList.split(",");
+
+        String subject = mEditTextSubject.getText().toString();
+        String message = mEditTextMessage.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(Intent.createChooser(intent, "Choose an email client"));
+    }
+}
